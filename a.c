77d83015e397e0fe3cadd2e2d2cc528b4e0f2dc8
mnieto-m <majanoyieto@gmail.com>
2024-08-25 20:54:12 +0200

    aaaaaa

--- a.c
+++ a.c
@@ -1,7 +1,73 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "./Libft/libft.h"
+#include <ctype.h>
+
+typedef struct e_automata
+{
+	int idx ;
+	int oidx ;
+	int ostate;//OLD_STATE, former char
+	int state ; 
+
+} t_automata;
+typedef enum e_states
+{
+	INIT,
+	ERR,
+	ISSPACE,
+	ISSIGN,
+	ISDIGIT,
+	EOLINE
+}	t_states;
+
+typedef struct e_data
+{
+	int i;
+	int nbr[256];
+}	t_data;
+
+void	print_data(t_data *data)
+{
+	int i = -1;
+	while (data->nbr[++i])
+		printf("[%d]\t%d\n", i, data->nbr[i]);
+}
+int ft_issign(char c)
+{
+	if(c == '+' || c == '-')
+		return(1);
+	return(0);
+}
+
+/* ----------------------------------------------------- */
+
+/* Accion en estado, cuando llega a state == 1 */
+void automata_error(int idx)
+{
+	printf("Parse error at idx %d\n", idx);
+	exit(1);
+}
+/** Accion en transicion de estado, guardado de informacion */
+void	automata_add_data(void *s, void *d, int oidx, int idx)
+{
+	char *str;
+	char *substr;
+	t_data *data;
+
+	str = s;
+	data = d;
+	substr = ft_substr(s, oidx, idx - oidx);
+	//protect malloc
+	data->nbr[data->i] = ft_atoi(substr);
+	data->i++;
+}
+
 int	automata_getstate(int ostate, int dict_idx)
 {
 	int matrix[5][4] = {
-		//s +-  A  ^
+		//s +-  D  ^
 		{2, 3, 4, 1}, // 0 INIT
 		{1, 1, 1, 1}, //1 ERR
 		{2, 3, 4, 1}, //2 ISSPACE
@@ -15,33 +81,64 @@ int	automata_getstate(int ostate, int dict_idx)
 // Devolver el estado de getstate
 int	automata_change_state(int ostate, char c)
 {
-	if (ft_isspace(c) != 0)
+	if (isspace(c) != 0)
 		return (automata_getstate(ostate, 0));
 	else if (ft_issign(c) != 0) 
 		return (automata_getstate(ostate, 1));
-	else if (ft_isnum(c) != 0)
+	else if (isdigit(c) != 0)
 		return (automata_getstate(ostate, 2));
 	else
 		return (automata_getstate(ostate, 3));
 }
 
-void	automata_parse(char *str)
+void	automata_parse(char *str, t_data *data)
 {
-	int idx;
-	int oidx;
-	int ostate; //OLD_STATE, former char
-	int state;  //STATE, new state, char at idx
+	int idx = 0;
+	int oidx = 0;
+	int ostate = 0; //OLD_STATE, former char
+	int state = 0;  //STATE, new state, char at idx
+	void	(*tsa[6][6])(void *, void *, int, int);
+	
+	//array de funciones, tsa[ostate][state]
+	memset(&tsa, 0, sizeof(tsa));
+	tsa[ISDIGIT][ISSPACE] = automata_add_data;
+	tsa[ISDIGIT][EOLINE] = automata_add_data;
 
 	idx = -1;
 	while (str[++idx])
 	{
+		//obtener estado actual de str[idx]
 		state = automata_change_state(ostate, str[idx]);
-		if (state == 1) //estado sencillo, llamaamos a error
-			//return pw_error()
-		else if (ostate == 4 && state == 2)//[DIGIT][SPACE]
-			//guardamos numero -> substr desde oidx hasta idx, itoa de substr, creas nuevo nodo con ese num
-		else if (state == 2)
-			//actualizas oidx
+
+		if (state == 1) //estado sencillo, llamamos a error
+			automata_error(idx);
+
+		//str //desde oidx hasta idx, itoa de substr, creas nuevo nodo con ese num
+		// else if ((state == 2 && ostate == 4) || 
+		// 		(state == 4 && str[idx + 1]) == '\0')
+		if (tsa[ostate][state] !=  NULL)
+		{
+			tsa[ostate][state]((void *)str, data, oidx, idx);
+			oidx = idx;
+		}
+		if (state == ISDIGIT && str[idx + 1] == '\0')
+			tsa[state][EOLINE]((void *)str, data, idx, idx + 1);
+
 		ostate = state; //antes de continuar la iteracion
 	}
+}
+/* ----------------------------------------------------- */
+
+/**
+ * ./a.out "234 +568 -321 3a34 " -> error
+ */
+
+int	main(int argc, char **argv)
+{
+	t_data data;
+
+	memset(&data, 0, sizeof(data));
+	automata_parse(argv[1], &data);
+	print_data(&data);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,16 +6,17 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 17:07:02 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/07/26 13:23:13 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2024/08/25 17:44:19 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(in argc, int **argv)
+int main(int argc, int **argv)
 {
-	if (argv)
-	{
-		
-	}
+	t_list *list_a;
+	t_list *list_b;
+	
+	if (argc = 1)
+		exit(1);
 }
