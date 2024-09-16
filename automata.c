6f317e6464da757c5dccb39dab5522080060ddc8
mnieto-m <majanoyieto@gmail.com>
2024-09-16 20:35:57 +0200

    aaaaa

--- automata.c
+++ automata.c
@@ -28,24 +28,26 @@ typedef struct s_feed_automata
 	int 	*num;
 	t_list	*new;
 }			t_feed_automata;
+
+
 void printList(t_list *a)
 {
 	t_list *lst;
 	lst = a;
 	while (lst != NULL) {
-		printf("%ls\n", ((int *)lst->content));
+		int nb = *(int *)lst->content;
+		printf("%i\n", nb);
 	lst = (lst)->next;
 	}
 }
 /* ----------------------------------------------------- */
 
 /* Accion en estado, cuando llega a state == 1 */
-void automata_error( t_list **a,t_automata *var_automata)
+void automata_error( t_list **a)
 {
-	printf("Parse error at idx %d\n", (var_automata)->idx);
+	printf("Parse error\n");
 	ft_lstclear(a, free);
-	free(var_automata);
-	exit(1);
+	exit(EXIT_FAILURE);
 }
 /** Accion en transicion de estado, guardado de informacion */
 void	automata_add_data(void *s, t_list **a, int oidx, int idx)
@@ -60,11 +62,11 @@ void	automata_add_data(void *s, t_list **a, int oidx, int idx)
 	if(feed.num== NULL)
 		return (ft_mfree(2,substr,feed));
 	*(feed.num) = ft_atoi_signal(substr);
+	free(substr);
 	feed.new = ft_lstnew((void *)feed.num);
 	if(feed.new== NULL)
 		return (ft_mfree(3,substr,feed.num,feed));
 	ft_lstadd_back((a), feed.new);
-	free(substr);
 }
 
 int	automata_getstate(int ostate, int dict_idx)
@@ -96,36 +98,35 @@ int	automata_change_state(int ostate, char c)
 // quitar puntero a var automata
 void	automata_parse(char *str, t_list **a)
 {
-	t_automata *var_automata;
+	t_automata var_automata;
 	
-	var_automata = ft_calloc(1, sizeof(t_automata));
+	ft_memset((void *)&var_automata, 0, sizeof(var_automata));
 	void	(*tsa[6][6])(void *, t_list **, int, int); //array de funciones, tsa[ostate][state]
 	ft_memset(tsa, 0, sizeof(tsa));	
 	tsa[ISDIGIT][ISSPACE] = automata_add_data;
 	tsa[ISDIGIT][EOLINE] = automata_add_data;
-	var_automata->idx = -1;
-	while (str[++(var_automata->idx)])
+	var_automata.idx = -1;
+	while (str[++(var_automata.idx)])
 	{
 		//obtener estado actual de str[idx]
-		var_automata->state = automata_change_state(var_automata->ostate, str[var_automata->idx]);
-		if (var_automata->state == 1) //estado sencillo, llamamos a error
-			automata_error(a, var_automata);
+		var_automata.state = automata_change_state(var_automata.ostate, str[var_automata.idx]);
+		if (var_automata.state == 1) //estado sencillo, llamamos a error
+			automata_error(a);
 		//str //desde oidx hasta idx, itoa de substr,
-		if (tsa[var_automata->ostate][var_automata->state] != NULL)
+		if (tsa[var_automata.ostate][var_automata.state] != NULL)
 		{
-			tsa[var_automata->ostate][var_automata->state]((void *)str, a, var_automata->oidx, var_automata->idx);
-			var_automata->oidx = var_automata->idx;
+			tsa[var_automata.ostate][var_automata.state]((void *)str, a, var_automata.oidx, var_automata.idx);
+			var_automata.oidx = var_automata.idx;
 		}
-		if (var_automata->state == ISDIGIT && str[var_automata->idx + 1] == '\0')
-			tsa[var_automata->state][EOLINE]((void *)str, (a), var_automata->oidx, (var_automata->idx) + 1);
-		var_automata->ostate = var_automata->state; //antes de continuar la iteracion
+		if (var_automata.state == ISDIGIT && str[var_automata.idx + 1] == '\0')
+			tsa[var_automata.state][EOLINE]((void *)str, (a), var_automata.oidx, (var_automata.idx) + 1);
+		var_automata.ostate = var_automata.state; //antes de continuar la iteracion
 	}
-	free(var_automata);
 }
 /* ----------------------------------------------------- */
 
 /**
- * ./a.out "234 +568 -321 3a34 " -> error
+ * ./a.out "234 +568 -321 3a34 " . error
  */
 
 
@@ -137,10 +138,8 @@ int	main(int argc, char **argv)
 	i = 1;
 	a = NULL;
 	while (i >= 1 && i < argc)
-	{
 		automata_parse(argv[i++], &a);
-	}
-
-ft_lstclear(&a, free);
+	printList(a);
+	ft_lstclear(&a, free);
 	return (0);
 }
