175df82ed286b772e4c733d2366e7778ef3ca01c
mnieto-m <majanoyieto@gmail.com>
2025-08-01 01:00:37 +0200

    now ok checker

--- src/init_stack.c
+++ src/init_stack.c
@@ -6,45 +6,50 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:39:44 by mnieto-m          #+#    #+#             */
-/*   Updated: 2025/07/30 16:26:25 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2025/08/01 01:00:22 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../include/push_swap.h"
 
-static int	get_value(t_list *a, int indx)
+void	fill_stack_nodes(t_list *a, t_node *nodes, int size)
 {
-	while (a->next && indx--)
+	int		i;
+	t_node	*curr;
+
+	i = 0;
+	curr = nodes;
+	while (i < size && a)
 	{
+		curr->value = *(int *)a->content;
+		if (i > 0)
+			curr->prev = curr - 1;
+		if (i < size - 1)
+			curr->next = curr + 1;
+		else
+			curr->next = nodes;
 		a = a->next;
+		curr++;
+		i++;
 	}
-	return (*(int *)(a->content));
+	nodes->prev = curr - 1;
 }
 
 t_stacks	*init_stack(t_list *a, int size)
 {
-	t_stacks	*stacks;
+	t_stacks	*st;
 
-	stacks = malloc((sizeof(t_stacks)) + size * sizeof(t_node));
-	if (stacks == NULL)
+	st = malloc(sizeof(t_stacks) + size * sizeof(t_node));
+	if (!st)
 		automata_error(&a);
-	ft_bzero(stacks, sizeof(t_stacks) + size * sizeof(t_node));
-	stacks->size = size;
-	stacks->a.size = size;
-	stacks->a.head = stacks->nodes;
-	stacks->a.name = 'a';
-	stacks->b.size = 0;
-	stacks->b.head = NULL;
-	stacks->b.name = 'b';
-	while (--size)
-	{
-		stacks->a.head->value = get_value(a, size);
-		stacks->a.head->next = stacks->a.head + 1;
-		(stacks->a.head + 1)->prev = stacks->a.head;
-		stacks->a.head++;
-	}
-	stacks->a.head->next = stacks->nodes;
-	stacks->nodes->prev = stacks->a.head;
-	stacks->a.head = stacks->nodes;
-	return (stacks);
+	ft_bzero(st, sizeof(t_stacks) + size * sizeof(t_node));
+	st->size = size;
+	st->a.size = size;
+	st->a.head = st->nodes;
+	st->a.name = 'a';
+	st->b.size = 0;
+	st->b.head = NULL;
+	st->b.name = 'b';
+	fill_stack_nodes(a, st->nodes, size);
+	return (st);
 }
